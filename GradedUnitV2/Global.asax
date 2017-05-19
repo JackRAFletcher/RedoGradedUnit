104cde1d1693a743b21afbf69806eacf80d1ecd6
jack <jack@JACK-WORK-PC>
2017-05-19 09:13:20 +0100

    Add project files.

---
+++ GradedUnitV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GradedUnitV2.MvcApplication" Language="C#" %>
---
+++ GradedUnitV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GradedUnitV2.Startup))]
+namespace GradedUnitV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
